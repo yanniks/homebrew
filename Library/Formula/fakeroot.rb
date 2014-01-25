3bef98b61a516a1f286500d2d1f20af841960beb
yanniks <kontakt@yanniks.de>
2014-01-25 21:53:55 +0100

    fakeroot 1.20

--- Library/Formula/fakeroot.rb
+++ Library/Formula/fakeroot.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fakeroot < Formula
   homepage 'http://packages.qa.debian.org/f/fakeroot.html'
-  url 'http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.18.4.orig.tar.bz2'
-  sha1 '60cdd12ea3a72f3676c0f3930ab908ff1f13b996'
+  url 'http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.20.orig.tar.bz2'
+  sha1 '3a8e90c77f44248c55333f73fdc8de4c3f3f5531'
 
   depends_on :autoconf
   depends_on :automake
